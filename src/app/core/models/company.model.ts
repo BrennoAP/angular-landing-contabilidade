c760e79933d41cc865890dc8e1cb0407733ead7d
Brenno <brennoap03@gmail.com>
2026-02-21 16:13:11 -0300

    feat: adicionar estrutura de dados da empresa e serviços

---
+++ src/app/core/models/company.model.ts
@@ -0,0 +1,35 @@
+export const COMPANY = {
+  name: 'contabilidade temp',
+  cnpj: 23322455555,
+  description: 'descrição da contabilidade',
+  address: 'rua temp 123',
+  city: 'cidade temp',
+  state: 'mg',
+  cep: 123456000,
+  phone: 3133345555,
+  whatsapp: 3133345555,
+  email: 'email@email.com',
+  socials: {
+    linked: 'www.linkdnl.com/temp',
+    instagram: '@insta',
+    facebook: 'www.facebook.com/temp',
+  },
+};
+
+export const COMPANY_SERVICES = {
+  aberturaEmpresa: {
+    title: 'Abertura de Empresas',
+    description: 'Comece seu negócio do jeito certo e sem burocracia.',
+    icon: 'business_center',
+  },
+  gestaoFiscal: {
+    title: 'Gestão Fiscal',
+    description: 'Planejamento tributário para você pagar o mínimo de impostos legalmente.',
+    icon: 'account_balance',
+  },
+  folhaPagamento: {
+    title: 'Folha de Pagamento',
+    description: 'Gestão completa de DP para sua equipe.',
+    icon: 'groups',
+  },
+};
